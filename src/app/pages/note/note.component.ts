0fd5b5b1646e9037d06eac00fe662d019f58e9b2
nkadebug <nka.debug@gmail.com>
2021-08-12 17:05:46 +0530

    update

--- src/app/pages/note/note.component.ts
+++ src/app/pages/note/note.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ElementRef, AfterViewInit, ViewChild, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  AfterViewInit,
+  ViewChild,
+  OnDestroy,
+} from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,15 +16,14 @@ import { Note } from 'src/app/model/note';
 import { IdbService } from 'src/app/services/idb.service';
 import { FormControl, FormGroup } from '@angular/forms';
 
-
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.scss']
+  styleUrls: ['./note.component.scss'],
 })
 export class NoteComponent implements OnInit, OnDestroy {
-
-  @ViewChild('canvas', { static: true }) canvas: any = document.querySelector('#canvas');
+  @ViewChild('canvas', { static: true }) canvas: any =
+    document.querySelector('#canvas');
 
   isCameraOpen = false;
   width = 640;
@@ -31,23 +37,21 @@ export class NoteComponent implements OnInit, OnDestroy {
     private db: AngularFireDatabase,
     private fs: AngularFirestore,
     private idb: IdbService
-  ) { }
+  ) {}
 
-  ngOnDestroy(): void {
-
-  }
+  ngOnDestroy(): void {}
 
   newNote = true;
-  noteId = "";
+  noteId = '';
   noteFrom = new FormGroup({
-    description: new FormControl('')
+    description: new FormControl(''),
   });
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       console.log(params);
-      this.newNote = params.id == "new";
-      if (params.id == "new") {
+      this.newNote = params.id == 'new';
+      if (params.id == 'new') {
         this.drawPlaceholder();
         this.openCamera();
       } else {
@@ -71,39 +75,42 @@ export class NoteComponent implements OnInit, OnDestroy {
       let _canvas = this.canvas.nativeElement;
       _canvas.width = _video.videoWidth;
       _canvas.height = _video.videoHeight;
-      _canvas.getContext("2d").drawImage(_video, 0, 0);
-      if (!this.noteId) this.noteId = Date.now() + "";
+      _canvas.getContext('2d').drawImage(_video, 0, 0);
+      if (!this.noteId) this.noteId = Date.now() + '';
     }
   }
-
-
 
   drawPlaceholder() {
     let _canvas = this.canvas.nativeElement;
     _canvas.width = this.height;
     _canvas.height = this.width;
-    let img = new Image;
+    let img = new Image();
     img.onload = () => {
-      _canvas.getContext('2d').drawImage(
-        img,
-        0.5 * (this.height - this.placeholderSize),
-        0.5 * (this.width - this.placeholderSize),
-        this.placeholderSize,
-        this.placeholderSize
-      )
+      _canvas
+        .getContext('2d')
+        .drawImage(
+          img,
+          0.5 * (this.height - this.placeholderSize),
+          0.5 * (this.width - this.placeholderSize),
+          this.placeholderSize,
+          this.placeholderSize
+        );
     };
-    img.src = "assets/bsi/camera.svg";
+    img.src = 'assets/bsi/camera.svg';
   }
 
+  currNote: Note | null = null;
+
   loadNote(id: string) {
-    this.idb.notes.get(id).then(note => {
+    this.idb.notes.get(id).then((note) => {
       if (note) {
+        this.currNote = note;
         if (note.photo) {
           let _canvas = this.canvas.nativeElement;
           _canvas.width = this.height;
           _canvas.height = this.width;
 
-          let img = new Image;
+          let img = new Image();
           img.onload = () => {
             _canvas.getContext('2d').drawImage(img, 0, 0);
           };
@@ -115,27 +122,36 @@ export class NoteComponent implements OnInit, OnDestroy {
   }
 
   saveNote() {
-
     let _canvas = this.canvas.nativeElement;
 
     let ts = Date.now();
-    let note: Note = {
-      id: this.noteId,
-      created_at: ts,
-      updated_at: ts,
-      photo: _canvas.toDataURL('image/jpeg', 0.5),
-      description: this.noteFrom.value.description,
-      uid: localStorage.uid,
-      trashed: false
+
+    if (this.currNote) {
+      this.currNote.updated_at = ts;
+      this.currNote.photo = this.captured
+        ? _canvas.toDataURL('image/jpeg', 0.5)
+        : null;
+      this.currNote.description = this.noteFrom.value.description;
+    } else {
+      this.currNote = {
+        id: this.noteId,
+        created_at: ts,
+        updated_at: ts,
+        photo: this.captured ? _canvas.toDataURL('image/jpeg', 0.5) : null,
+        description: this.noteFrom.value.description,
+        uid: localStorage.uid,
+        trashed: false,
+      };
     }
 
-    this.idb.notes.put(note).then(() => {
-      this.router.navigate([""]);
-    }).catch((e) => {
-      console.log(e)
-    });
-
-
+    this.idb.notes
+      .put(this.currNote)
+      .then(() => {
+        this.router.navigate(['']);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
 
     // _canvas.toBlob((blob:any)=>{
     //   console.log(blob.size);
@@ -148,18 +164,23 @@ export class NoteComponent implements OnInit, OnDestroy {
   }
 
   trashNote() {
-    this.idb.notes.get(this.noteId).then(note => {
-      if (note) {
-        note.trashed = true;
-        this.idb.notes.put(note).then(() => {
-          this.router.navigate([""]);
-        }).catch((e) => {
-          console.log(e)
-        });
-      }
-    }).catch((e) => {
-      console.log(e)
-    });
+    this.idb.notes
+      .get(this.noteId)
+      .then((note) => {
+        if (note) {
+          note.trashed = true;
+          this.idb.notes
+            .put(note)
+            .then(() => {
+              this.router.navigate(['']);
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
-
 }
